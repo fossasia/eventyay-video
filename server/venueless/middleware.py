9254dd31d4166ee0f324084ce125a809b6882753
Saksham Sirohi <sakshamsirohi27@gmail.com>
2025-11-14 13:15:20 +0530

    bug: youtube error fix (#432)
    * youtube error fix
    
    * implemented suggestions

--- server/venueless/middleware.py
+++ server/venueless/middleware.py
@@ -1,20 +1,17 @@
 from django.utils.deprecation import MiddlewareMixin
 
+REFERRER_POLICY = "strict-origin-when-cross-origin"
+
 
 class XFrameOptionsMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        # Don't set it if it's already in the response
-        if response.get("X-Frame-Options") is not None:
-            return response
+        has_xfo = response.get("X-Frame-Options")
+        is_exempt = getattr(response, "xframe_options_exempt", False)
+        is_zoom = request.path.startswith("/zoom")
 
-        # Don't set it if they used @xframe_options_exempt
-        if getattr(response, "xframe_options_exempt", False):
-            return response
+        if has_xfo is None and not is_exempt and not is_zoom:
+            response["X-Frame-Options"] = "DENY"
 
-        # Don't set for zoom app
-        # We don't use xframe_options_exempt here since that doesn't catch error pages
-        if request.path.startswith("/zoom"):
-            return response
-
-        response["X-Frame-Options"] = "DENY"
+        if "Referrer-Policy" not in response:
+            response["Referrer-Policy"] = REFERRER_POLICY
         return response
